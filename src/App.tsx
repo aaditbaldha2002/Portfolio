c0ef397fe002c9d434fc89ee553ccceabddda417
Aadit Harshal Baldha <aaditbaldha2002@gmail.com>
2025-03-08 19:39:59 -0500

    added cool animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import { theme } from './lib/theme/theme';
 import { IntroSection } from './lib/components/IntroSection';
-
+import '@fontsource/kanit/700.css';
 export const AppContext = React.createContext<{ welcomeBtnClicked: boolean }>({
   welcomeBtnClicked: false,
 });
@@ -23,15 +23,12 @@ const App: React.FC = (): ReactNode => {
 
 const AppWrapper = styled.div`
   display: flex;
-  background: ${(props) =>
-    `linear-gradient(to bottom, ${props.theme.black}, ${props.theme.light_black})`};
   width: 100%;
   height: auto;
   align-items: center;
   justify-content: center;
   font-family: 'Orbitron', sans-serif;
   overflow-x: hidden;
-  /* overflow-y: clip; */
   /* overflow-x: clip; */
 `;
 
--- src/lib/components/IntroSection.tsx
+++ src/lib/components/IntroSection.tsx
@@ -11,56 +11,144 @@ export const IntroSection: React.FC<IntroSectionProps> = (props) => {
   const name = props.name;
   const theme = useTheme();
   const [nameAnimationEnded, setNameAnimationEnded] = React.useState(false);
+
+  const [gateOpened, setGetOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setGetOpened(true);
+    }, 1500);
+    setTimeout(() => {});
+  }, [gateOpened]);
   return (
     <Wrapper>
-      <DpWrapper data-testid="Dp-wrapper-test-id">
-        <DpImage data-testid="Dp-img-test-id" />
-      </DpWrapper>
-      <InfoWrapper data-testid="Info-wrapper-test-id">
-        <NameWrapper
-          onAnimationEnd={() => setNameAnimationEnded(true)}
-          data-testid="Name-wrapper-test-id"
-        >
-          <TextTyper
-            text={name}
-            size="45px"
-            weight="bold"
-            color={theme.white}
-            margin="30px"
-            data-testid="Name-texttyper-test-id"
-          />
-        </NameWrapper>
-        <SummaryWrapper
-          fadeAnimate={nameAnimationEnded}
-          data-testid="summaryWrapper-test-id"
-        >
-          I am a passionate Web Developer and Machine Learning student,
-          constantly exploring the intersection of frontend development and
-          AI-driven solutions. With a strong foundation in React.js, JavaScript,
-          and modern web technologies, I build seamless user experiences while
-          also delving into the world of machine learning to create intelligent
-          and data-driven applications.
-        </SummaryWrapper>
-      </InfoWrapper>
+      <GateWrapper id="gatewrapper-id">
+        <LeftGate />
+        <RightGate />
+      </GateWrapper>
+      <ContentWrapper gateOpened={gateOpened}>
+        <DpWrapper data-testid="Dp-wrapper-test-id">
+          <DpImage data-testid="Dp-img-test-id" />
+        </DpWrapper>
+        <InfoWrapper data-testid="Info-wrapper-test-id">
+          <NameWrapper
+            data-testid="Name-wrapper-test-id"
+            gateOpened={gateOpened}
+          >
+            <TextTyper
+              text={name}
+              size="45px"
+              weight="normal"
+              color={theme.white}
+              margin="30px"
+              data-testid="Name-texttyper-test-id"
+            />
+          </NameWrapper>
+          <SummaryWrapper
+            gateOpened={gateOpened}
+            data-testid="summaryWrapper-test-id"
+          >
+            I am a passionate Web Developer and Machine Learning student,
+            constantly exploring the intersection of frontend development and
+            AI-driven solutions. With a strong foundation in React.js,
+            JavaScript, and modern web technologies, I build seamless user
+            experiences while also delving into the world of machine learning to
+            create intelligent and data-driven applications.
+          </SummaryWrapper>
+        </InfoWrapper>
+      </ContentWrapper>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
   width: 100%;
-  padding: 2em;
-  background: url(${introBg});
+  /* background: url(${introBg}); */
+  background: ${(props) =>
+    `linear-gradient(to left,${props.theme.darkest_blue},${props.theme.black})`};
   background-size: cover;
   background-position: center;
   display: flex;
   flex-direction: row;
   gap: 1em;
-
+  overflow-y: clip;
   @media (max-width: 640px) {
     flex-direction: column;
     align-items: center;
     padding: 1em;
   }
+`;
+
+const fadeIn = keyframes`
+  from{
+    opacity:0;
+  }to{
+    opacity:1;
+  }
+`;
+
+const GateWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  animation: 0.25s ${fadeIn} linear forwards;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  height: 311px;
+`;
+
+const goLeft = keyframes`
+  from{
+    transform: translateX(0%);
+  }to{
+    transform: translateX(-100%);
+  }
+`;
+
+const LeftGate = styled.div`
+  display: flex;
+  background: ${(props) => `linear-gradient(
+    -135deg,
+    ${props.theme.gate_blue},
+    ${props.theme.black} 90%
+  )`};
+  width: 50vw;
+  align-self: stretch;
+  animation: 1s ${goLeft} ease-in forwards;
+  box-shadow: -10px 0 30px ${(props) => props.theme.purple};
+`;
+
+const goRight = keyframes`
+  from{
+    transform: translateX(0%);
+  }to{
+    transform: translateX(100%);
+  }
+`;
+
+const RightGate = styled.div`
+  display: flex;
+  background: ${(props) => `linear-gradient(
+    135deg,
+    ${props.theme.gate_blue},
+    ${props.theme.black} 90%
+  )`};
+  width: 50vw;
+  align-self: stretch;
+  animation: 1s ${goRight} ease-in forwards;
+  box-shadow: 10px 0 30px ${(props) => props.theme.purple};
+`;
+
+const ContentWrapper = styled.div<{ gateOpened: boolean }>`
+  display: flex;
+  padding: 2em;
+  opacity: 0;
+  ${(props) =>
+    props.gateOpened &&
+    css`
+      animation: 0.15s ${fadeIn} linear forwards;
+    `};
 `;
 
 const DpWrapper = styled.div`
@@ -76,12 +164,6 @@ const DpWrapper = styled.div`
     margin-right: 0em;
   }
 `;
-const fadeIn = keyframes`
-  from{
-    opacity:0;
-  }to{
-    opacity:1;
-  }`;
 
 const DpImage = styled.div`
   width: 228px;
@@ -91,42 +173,95 @@ const DpImage = styled.div`
   background-repeat: no-repeat;
   background-position: center;
   border-radius: 50%;
-  border: ${(props) => `5px solid ${props.theme.white}`};
-  animation: 1.5s ${fadeIn} ease-out forwards;
+  border: ${(props) => `5px solid ${props.theme.purple}`};
+  animation: auraPulse 2s infinite alternate;
+  @keyframes auraPulse {
+    from {
+      transform: scale(1);
+      box-shadow: 0 0 40px 5px ${(props) => props.theme.purple};
+      border: ${(props) => `5px solid ${props.theme.purple}`};
+    }
+    to {
+      transform: scale(1.05);
+      box-shadow: 0 0 60px 10px ${(props) => props.theme.gate_blue};
+      border: ${(props) => `5px solid ${props.theme.gate_blue}`};
+    }
+  }
 `;
 
 const InfoWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 75%;
+  padding: 0em 2em;
 `;
 
-const leftToRight = keyframes`
-  from {
-    transform: translateX(100%)
-  }
-  to {
-    transform: translateX(0);
-  }
-`;
-
-const NameWrapper = styled.div`
+const NameWrapper = styled.div<{ gateOpened: boolean }>`
   position: relative;
-  animation: 0.75s ${leftToRight} ease-out forwards;
+  /* text-shadow:
+    0px 0px 20px ${(props) => props.theme.light_blue},
+    0px 0px 20px ${(props) => props.theme.light_blue},
+    0px 0px 10px ${(props) => props.theme.light_blue}; */
   @media (max-width: 640px) {
     text-align: center;
   }
+  ${(props) =>
+    props.gateOpened &&
+    css`
+      animation: textGlitch 1s linear forwards;
+    `}
+
+  @keyframes textGlitch {
+    10% {
+      text-shadow: -10px -10px 0px ${(props) => props.theme.red};
+    }
+    11% {
+      text-shadow: 0px 0px 0px;
+    }
+    15% {
+      text-shadow: -10px 10px 0px ${(props) => props.theme.light_blue};
+    }
+    16% {
+      text-shadow: 0px 0px 0px;
+    }
+    100% {
+      text-shadow:
+        0px 0px 20px ${(props) => props.theme.light_blue},
+        0px 0px 20px ${(props) => props.theme.light_blue},
+        0px 0px 10px ${(props) => props.theme.light_blue};
+    }
+  }
 `;
 
-const SummaryWrapper = styled.div<{ fadeAnimate: boolean }>`
+const SummaryWrapper = styled.div<{ gateOpened: boolean }>`
   font-size: 25px;
   color: ${(props) => props.theme.white};
   margin-bottom: 20px;
   position: relative;
-  opacity: 0;
   ${(props) =>
-    props.fadeAnimate &&
+    props.gateOpened &&
     css`
-      animation: 1.5s ${fadeIn} linear forwards;
+      animation: textGlitch 1s linear forwards;
     `}
+
+  @keyframes textGlitch {
+    10% {
+      text-shadow: -10px -10px 0px ${(props) => props.theme.red};
+    }
+    11% {
+      text-shadow: 0px 0px 0px;
+    }
+    15% {
+      text-shadow: -10px 10px 0px ${(props) => props.theme.light_blue};
+    }
+    16% {
+      text-shadow: 0px 0px 0px;
+    }
+    100% {
+      text-shadow:
+        0px 0px 20px ${(props) => props.theme.light_blue},
+        0px 0px 20px ${(props) => props.theme.light_blue},
+        0px 0px 10px ${(props) => props.theme.light_blue};
+    }
+  }
 `;
